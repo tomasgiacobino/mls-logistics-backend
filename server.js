fea80f7ac1d903bb3ce63f70a50eb1e3825e44e1
tomas <tomas.giacobino@gmail.com>
2025-10-28 20:56:42 -0300

    Cambios backend
    Cambios

--- server.js
+++ server.js
@@ -1,45 +1,48 @@
 const express = require('express');
-const mercadopago = require('mercadopago');
-const dotenv = require('dotenv');
+const {MercadoPagoConfig, Preference} =  require('mercadopago');
+const cors = require('cors');
 
-dotenv.config(); // Carga las variables de entorno del archivo .env
+require('dotenv').config(); // Carga las variables de entorno del archivo .env
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Configuración de Mercado Pago
-mercadopago.configure({
-    access_token: process.env.MERCADOPAGO_ACCESS_TOKEN
+const client = new MercadoPagoConfig({ 
+    accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
+    sandbox: true
 });
+const preference = new Preference(client);
 
 // Middleware para procesar JSON en las solicitudes
 app.use(express.json());
+app.use(cors());
 
 // Endpoint para crear la preferencia de pago
 app.post('/create-order', async (req, res) => {
     try {
-        const preference = {
-            items: [
-                {
-                    title: 'Asesoramiento personalizado',
-                    unit_price: 100, // Precio del asesoramiento
-                    quantity: 1,
-                }
-            ],
-            back_urls: {
-                success: 'http://localhost:3000/success',
-                failure: 'http://localhost:3000/failure',
-                pending: 'http://localhost:3000/pending'
-            },
-            auto_return: 'approved'
-        };
-
-        const response = await mercadopago.preferences.create(preference);
-        res.status(200).json({ id: response.body.id });
-
+        const result = await preference.create({
+            body: {
+                items: [
+                    {
+                        title: 'Asesoramiento personalizado',
+                        unit_price: 100,
+                        quantity: 1,
+                    }
+                ],
+                back_urls: {
+                    success: "https://1397933b5c5c.ngrok-free.app/success",
+                    failure: "https://1397933b5c5c.ngrok-free.app/failure",
+                    pending: "https://1397933b5c5c.ngrok-free.app/pending",
+                },
+                auto_return: "approved"
+            }
+        });
+        console.log('Respuesta de la API de mercado pago:', result)
+        res.status(200).json({ id: result.id, sandbox_init_point: result.sandbox_init_point });
     } catch (error) {
-        console.error('Error al crear la preferencia de pago:', error);
-        res.status(500).json({ error: 'Error interno del servidor' });
+        console.error('Error al crear la preferencia de pago:', error.response?.data || error.message);
+        res.status(500).json({ error: 'Error interno del servidor', details: error.response?.data });
     }
 });
 
